8f64440ccf77aa7badb22751122ce35029ee30a8
worapolburaphan <worapol.pan.buraphan@gmail.com>
2025-12-07 01:20:46 +0700

    config: add allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()]
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	preview: {
+		allowedHosts: ['sau-os-quiz.up.railway.app', 'localhost', '127.0.0.1']
+	}
 });
